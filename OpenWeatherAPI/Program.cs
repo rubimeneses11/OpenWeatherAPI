1e099aa9a9e1116d3d50da76a68c45a0b987d040
Rubi Meneses <rubi.ventura@icloud.com>
2022-09-12 10:32:28 -0500

    completed exercise

---
+++ OpenWeatherAPI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using OpenWeatherAPI;
+
+string key = File.ReadAllText("appsettings.json");
+
+//store API key as a variable
+string APIkey = JObject.Parse(key).GetValue("APIkey").ToString();
+
+
+
+//prompting user for input
+Console.WriteLine($"Please enter your zipcode");
+//storing zipcode as a variable
+var zipCode = Console.ReadLine();
+
+
+//calling weather API
+var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIkey}";
+
+//to separate space
+Console.WriteLine();
+
+
+//RESULT:
+Console.WriteLine($"It is currently {WeatherMap.GetTemp(weatherURL)} degrees F in your location.");
---
+++ OpenWeatherAPI/WeatherMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherAPI
+{
+    public class WeatherMap
+    {
+        public static double GetTemp(string weatherURL)
+        {
+            var client = new HttpClient();
+
+            var weatherResponse = client.GetStringAsync(weatherURL).Result;
+
+            var temp = double.Parse(JObject.Parse(weatherResponse)["main"]["temp"].ToString());
+
+             return temp;
+        }
+    }
+}
+
